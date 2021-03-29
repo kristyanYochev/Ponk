64ad6678216cb50ba72441f88e79c35ad4c7f9de
Kristyan Yochev <kris.yochev@gmail.com>
2021-03-29 17:14:01 +0300

    Created window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Ponk");
+
+    while (window.isOpen())
+    {
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return 0;
 }
