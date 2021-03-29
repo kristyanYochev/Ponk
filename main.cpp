6a74730d551fbbdbd6de2d2e8682592429f1ea44
Kristyan Yochev <kris.yochev@gmail.com>
2021-03-29 17:28:00 +0300

    Added paddle with basic movement

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Ponk");
 
+    sf::RectangleShape paddle(sf::Vector2f(10.f, 100.f));
+    paddle.setPosition(0, 0);
+    const float paddleMaxSpeed = 2000.f;
+    float paddleSpeed;
+
+    sf::Clock clk;
+
     while (window.isOpen())
     {
         sf::Event event{};
@@ -12,7 +19,15 @@ int main()
             if (event.type == sf::Event::Closed) window.close();
         }
 
+        paddleSpeed = 0;
+        paddleSpeed += sf::Keyboard::isKeyPressed(sf::Keyboard::S) ? paddleMaxSpeed : 0;
+        paddleSpeed += sf::Keyboard::isKeyPressed(sf::Keyboard::W) ? -paddleMaxSpeed : 0;
+
+        paddle.move(0, paddleSpeed * clk.getElapsedTime().asSeconds());
+
+        clk.restart();
         window.clear();
+        window.draw(paddle);
         window.display();
     }
 
